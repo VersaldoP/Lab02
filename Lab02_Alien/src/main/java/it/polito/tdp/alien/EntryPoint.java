d484b28b0c6f6b1d832c7007cb8e90a4743db6d4
pietr <pietr@LAPTOP-FCSJ88N2>
2021-03-15 21:26:19 +0100

    impostato la gerarchia mvc-pattern

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/EntryPoint.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/EntryPoint.java
@@ -2,6 +2,8 @@ package it.polito.tdp.alien;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import it.polito.tdp.alien.model.Dictionary;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,7 +14,17 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+    	/*
+    	 * cambio il caricamento creando l'oggetto loader e chiamando load su di esso 
+    	 * per aver la possibilità di richiamare direttamente il controller
+    	 * e settare il model con il metodo generato nel FXMLcontroller 
+    	 */
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
+        FXMLController controller= loader.getController();
+        
+        Dictionary model = new Dictionary();
+        controller.setModel(model);
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -2,12 +2,15 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.alien.model.Dictionary;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	private Dictionary model;
 
     @FXML
     private ResourceBundle resources;
@@ -26,6 +29,10 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    public void setModel(Dictionary model) {
+		// TODO Auto-generated method stub
+		this.model=model;
+	}
 
     @FXML
     void doReset(ActionEvent event) {
@@ -45,4 +52,6 @@ public class FXMLController {
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
+
+	
 }
